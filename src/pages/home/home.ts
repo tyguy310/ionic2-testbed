5f3b8ee2f8620bcf73e01ed739840b3df616ce12
tyguy310 <tyler_maier@yahoo.com>
2017-01-24 12:43:10 -0700

    test passes for setting title

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+// import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -8,8 +8,14 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-    
+  title: string = "My Page";
+
+  constructor() {
+
   }
+
+  changeTitle(title){
+    this.title = title;
+}
 
 }
